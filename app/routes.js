130ccb63b9a4107bf3e40b566af07fc37cbb2144
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-03-09 10:16:41 +0000

    Add routes to job search and api

--- app/routes.js
+++ app/routes.js
@@ -7,15 +7,24 @@ router.get('/', function (req, res) {
 
 });
 
-// Route JSON job data to template variables and script string
-router.get('/search-open-jobs/job/:id', function(req, res) {
+router.get('/open-jobs/search', function(req, res) {
   var fs = require('fs');
   var data = fs.readFileSync(__dirname + '/assets/data/jobs.json', 'utf-8');
       data = JSON.parse(data);
 
-  res.render('search-open-jobs/job', {
+  res.render('open-jobs/search', {jobs: data.jobs});
+});
+
+// Route JSON job data to template variables and script string
+router.get('/open-jobs/job/:id', function(req, res) {
+  var fs = require('fs');
+  var data = fs.readFileSync(__dirname + '/assets/data/jobs.json', 'utf-8');
+      data = JSON.parse(data);
+
+  res.render('open-jobs/job', {
     job : data.jobs[req.params.id],
-    jobString: JSON.stringify(data.jobs[req.params.id])
+    jobString : JSON.stringify(data.jobs[req.params.id]),
+    jobID : [req.params.id]
   });
 });
 
